d441b1a959e7c949b905dd4346596acbeb44d3a0
Harshithb23me1023 <141806149+Harshithb23me1023@users.noreply.github.com>
2025-04-04 16:13:33 +0530

    FileManager.h

--- FileManager.h
+++ FileManager.h
@@ -1,21 +1,29 @@
-#ifndef FILE_MANAGER_H
-#define FILE_MANAGER_H
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include "Stock.h"
-#include "RedBlackTree.h"
 
 using namespace std;
 
 class FileManager {
 public:
-    // Save stocks to a file
-    static void saveToFile(const string& filename, RedBlackTree& tree);
-
-    // Load stocks from a file
-    static void loadFromFile(const string& filename, RedBlackTree& tree);
+    FileManager();
+    ~FileManager();
+    
+    bool saveToFile(const string& filename, const vector<Stock>& stocks);
+    bool loadFromFile(const string& filename, vector<Stock>& stocks);
+    bool fileExists(const string& filename);
+    void clearFile(const string& filename);
+    
+    void printFileContents(const string& filename);
+    bool validateFileFormat(const string& filename);
+    void logError(const string& errorMessage);
+    
+private:
+    void createBackup(const string& filename);
 };
 
-#endif
+#endif // FILEMANAGER_H
