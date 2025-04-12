f8ba7df3549a023b934c375649d2dd7545be96c6
Harshithb23me1023 <141806149+Harshithb23me1023@users.noreply.github.com>
2025-04-12 05:42:02 +0530

    Update RedBlackTree.h

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
 
 enum Color { RED, BLACK };
@@ -12,53 +11,59 @@ enum Color { RED, BLACK };
 struct Node {
     string stockSymbol;
     double stockPrice;
-    Node* left;
-    Node* right;
-    Node* parent;
+    Node *left, *right, *parent;
     Color color;
 
-    Node(string symbol, double price) {
-        stockSymbol = symbol;
-        stockPrice = price;
-        left = right = parent = nullptr;
-        color = RED;
-    }
+    Node(string symbol, double price)
+        : stockSymbol(symbol), stockPrice(price), left(nullptr), right(nullptr), parent(nullptr), color(RED) {}
 };
 
 class RedBlackTree {
 private:
-    Node* root;
-    Node* NIL;
+    Node *root, *NIL;
 
-    // Helper functions
     void rotateLeft(Node* node);
     void rotateRight(Node* node);
     void fixInsert(Node* node);
     void fixDelete(Node* node);
     void transplant(Node* u, Node* v);
-    Node* minimum(Node* node);
-    void deleteTree(Node* node);
+    void deleteNode(Node* node, const string& symbol);
+    Node* minimum(Node* node) const;
+
     void inorderHelper(Node* node) const;
     void printTreeHelper(Node* node, string indent, bool last) const;
+    void deleteTree(Node* node);
+
     void saveHelper(Node* node, ofstream& file) const;
-    
+    int countStocks(Node* node) const;
+    double calculateTotalStockValue(Node* node) const;
+    Node* findMinStock(Node* node) const;
+    Node* findMaxStock(Node* node) const;
+
 public:
-    // Constructor/Destructor
     RedBlackTree();
     ~RedBlackTree();
 
-    // Core operations
     void insert(string symbol, double price);
-    void remove(string symbol);
+    void remove(string stockSymbol);
     Node* search(string symbol) const;
-    
-    // Display functions
-    void displayInOrder() const { inorderHelper(root); }
-    void displayTreeStructure() const { printTreeHelper(root, "", true); }
-    
-    // File operations
-    void saveToFile(string filename) const;
-    void loadFromFile(string filename);
+    void searchStock(string stockSymbol);
+    void updateStockPrice(string stockSymbol, double newPrice);
+
+    void displayInOrder() const;
+    void displayTreeStructure() const;
+
+    void saveToFile(ofstream& file) const;
+    void saveToFile(const string& filename) const;
+    void loadFromFile(const string& filename);
+
+    int getTotalStocks() const;
+    double getTotalStockValue() const;
+    double calculateAverageStockPrice() const;
+    void displayStockStatistics() const;
+
+    Node* getMaxStock() const;
+    Node* getMinStock() const;
 };
 
 #endif
