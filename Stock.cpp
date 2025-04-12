867e89d7efc9073d1697137041b9c709c5e26c5f
Harshithb23me1023 <141806149+Harshithb23me1023@users.noreply.github.com>
2025-04-12 05:42:21 +0530

    Update Stock.cpp

--- Stock.cpp
+++ Stock.cpp
@@ -1,58 +1,30 @@
 #include "Stock.h"
-#include <iostream>
-using namespace std;
 
-// Constructor
-Stock::Stock(string symbol, double price, string name, string sec) {
-    stockSymbol = symbol;
-    stockPrice = price;
-    companyName = name;
-    sector = sec;
-}
+Stock::Stock(string symbol, double price, string name, string sector)
+    : stockSymbol(symbol), stockPrice(price), companyName(name), sector(sector) {}
 
-// Getters
-string Stock::getStockSymbol() {
-    return stockSymbol;
-}
+string Stock::getStockSymbol() { return stockSymbol; }
+double Stock::getStockPrice() { return stockPrice; }
+string Stock::getCompanyName() const { return companyName; }
+string Stock::getSector() const { return sector; }
 
-double Stock::getStockPrice() {
-    return stockPrice;
-}
+void Stock::setStockPrice(double price) { stockPrice = price; }
+void Stock::setCompanyName(string name) { companyName = name; }
+void Stock::setSector(string sec) { sector = sec; }
 
-string Stock::getCompanyName() const {
-    return companyName;
-}
-
-string Stock::getSector() const {
-    return sector;
-}
-
-// Setters
-void Stock::setStockPrice(double price) {
-    stockPrice = price;
-}
-
-void Stock::setCompanyName(string name) {
-    companyName = name;
-}
-
-void Stock::setSector(string sec) {
-    sector = sec;
-}
-
-// Display
-void Stock::displayStock() const {
-    cout << "Stock Symbol: " << stockSymbol << "\n";
-    cout << "Company Name: " << companyName << "\n";
-    cout << "Sector: " << sector << "\n";
-    cout << "Stock Price: $" << stockPrice << "\n";
-}
-
-// Comparison
 bool Stock::compareStock(Stock otherStock) {
-    return this->stockPrice > otherStock.getStockPrice();
+    return stockSymbol == otherStock.stockSymbol;
 }
 
-bool Stock::isSector(string sec) {
-    return this->sector == sec;
+bool Stock::isSector(string sector) {
+    return this->sector == sector;
+}
+
+// 🆕 Add this display function
+void Stock::displayStock() const {
+    cout << "Symbol: " << stockSymbol << endl;
+    cout << "Company Name: " << companyName << endl;
+    cout << "Sector: " << sector << endl;
+    cout << "Price: " << stockPrice << endl;
+    cout << "--------------------------" << endl;
 }
